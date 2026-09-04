84b4298b805fcdcc845ea7425a749572f8734393
Artem Belikin <artemb@vt.edu>
2026-09-04 13:25:35 -0400

    added the requirements file and the test file to ensure everything works

---
+++ Testing.py
@@ -0,0 +1,20 @@
+import numpy
+import pandas
+import scipy
+
+import networkx
+import geopandas
+import folium
+
+import matplotlib
+
+import sklearn
+import statsmodels
+
+import openpyxl
+import requests
+
+import jupyter_core
+import ipykernel
+
+print("Everything is Working!")
